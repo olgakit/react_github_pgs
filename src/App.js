7df2be9932437b0bd1c9276967ec1e6aea485873
Camp Fire <campfire@mac-mini.attlocal.net>
2018-10-15 21:54:23 -0500

    Markers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import SquareAPI from './API/index';
+import SquareAPI from './API/';
 import Map from './components/Map';
+import Sidebar from './components/Sidebar';
 
 class App extends Component {
   constructor() {
@@ -10,14 +11,42 @@ class App extends Component {
       venues: [],
       markers: [],
       center: [],
-      zoom: 12
+      zoom: 12.9,
+      styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#ff0200"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"labels.text","stylers":[{"visibility":"on"},{"saturation":"-3"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#748ca3"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ff0200"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ff0200"},{"saturation":"23"},{"lightness":"20"},{"visibility":"off"}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"color":"#ffdbda"},{"saturation":"0"},{"visibility":"on"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#ff0200"},{"saturation":"100"},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#f39247"},{"saturation":"0"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#008eff"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#ffe5e5"},{"saturation":"0"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#ff0200"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}],
+      updateSuperState: obj => {
+        this.setState(obj);
+      }
     }
+  }
+  
+  closeAllMarkers = () => {
+    const markers = this.state.markers.map(marker => {
+      marker.isOpen = false;
+      return marker;
+    });
+    this.setState({ markers: Object.assign(this.state.markers, markers) });
+  }
+  handleMarkerClick = marker => {
+    this.closeAllMarkers();
+    marker.isOpen = true;
+    this.setState({ markers: Object.assign(this.state.markers, marker) });
+    const venue = this.state.venues.find(venue => venue.id === marker.id);
+
+    SquareAPI.getVenueDetails(marker.id).then( res => {
+      const newLocation = Object.assign(venue, res.response.venue);
+      this.setState({venues: Object.assign(this.state.venues, newLocation)});
+    });
+  }
+
+  handleOneLocationClick = venue => {
+    const marker = this.state.markers.find(marker => marker.id === venue.id);
+    this.handleMarkerClick(marker);
   }
 
   componentDidMount() {
     SquareAPI.search({
       near: "Moscow, Russia",
-      query: "parks",
+      query: "музеи",
       limit: 10
     }).then(results => {
       const { venues } = results.response;
@@ -27,7 +56,8 @@ class App extends Component {
           lat: venue.location.lat,
           lng: venue.location.lng,
           isOpen: false,
-          isVisible: true
+          isVisible: true,
+          id: venue.id
         };
       });
       this.setState({venues, center, markers});
@@ -37,7 +67,8 @@ class App extends Component {
   render() {
     return(
       <div className="App"> 
-        <Map {...this.state} />
+        <Sidebar {...this.state} handleOneLocationClick = {this.handleOneLocationClick}/>
+        <Map {...this.state} handleMarkerClick = {this.handleMarkerClick}/>
       </div>
     );
   }
